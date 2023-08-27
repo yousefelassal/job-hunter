e2761a114441f1ed53719d060e6a31497663142c
Yousef <yousefsameh24@gmail.com>
2023-08-28 02:18:07 +0300

    link components

--- app/index.js
+++ app/index.js
@@ -32,6 +32,8 @@ export default function Home() {
                     }}
                 >
                     <Welcome />
+                    <Popularjobs />
+                    <Nearbyjobs />
                 </View>
             </ScrollView>
         </SafeAreaView>
